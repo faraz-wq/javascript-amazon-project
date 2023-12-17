f82d00198f83f0000390a75454980e80eb87da42
farazpf12@gmail.com <91552196+M1sm4tch@users.noreply.github.com>
2023-12-17 19:46:51 +0530

    commit 2 generate html with js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,7 +2,7 @@ const products = [{
     image:"images/products/athletic-cotton-socks-6-pairs.jpg",
     name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
     rating: {
-        stars:"images/ratings/rating-45.png",
+        stars: 4.5,
         count: 87
     },
     priceCents : 1090
@@ -10,7 +10,7 @@ const products = [{
     image:"images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating:{
-        stars: "images/ratings/rating-40.png",
+        stars: 4,
         count: 127
     },
     priceCents: 2095
@@ -18,34 +18,36 @@ const products = [{
     image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating:{
-        stars: "images/ratings/rating-45.png",
+        stars: 4.5,
         count: 56
     },
     priceCents: 799
 }];
 
+let productsHTML ='';
+
 products.forEach(product => {
-    const html = `
+    productsHTML+= `
             <div class="product-container">
             <div class="product-image-container">
             <img class="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+                src="${product.image}">
             </div>
 
             <div class="product-name limit-text-to-2-lines">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            ${product.name}
             </div>
 
             <div class="product-rating-container">
             <img class="product-rating-stars"
-                src="images/ratings/rating-45.png">
+                src="images/ratings/rating-${product.rating.stars *10}.png">
             <div class="product-rating-count link-primary">
-                87
+                ${product.rating.count}
             </div>
             </div>
 
             <div class="product-price">
-            $10.90
+            $${(product.priceCents/100).tofixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -75,5 +77,9 @@ products.forEach(product => {
             </button>
         </div>
     `
-    console.log(html);
+    
 });
+console.log(productsHTML);
+
+document.querySelector('.js-products-grid').
+innerHTML = productsHTML;
