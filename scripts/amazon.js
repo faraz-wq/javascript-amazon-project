d11e9631ea8a0f4901b85ff6433c93ea570867b6
farazpf12@gmail.com <91552196+M1sm4tch@users.noreply.github.com>
2023-12-17 20:25:14 +0530

    commit 4 items can now be added to cart array

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach(product => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
@@ -57,3 +58,29 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').
+    forEach((button) =>{
+        button.addEventListener('click',()=>{
+            const productId = button.dataset.productId;
+            
+            let matchingItem;
+
+            cart.forEach((item) => {
+                if (productId === item.productId ) {
+                    matchingItem = item;
+                } 
+            });
+
+            if(matchingItem)    {
+                matchingItem.Quantity+=1;
+            } else {
+                cart.push({
+                    productId: productId,
+                    Quantity:1
+                });
+            }
+            console.log(cart);
+        });        
+    })
+    
